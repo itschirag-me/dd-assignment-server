a1d4bb19f24cd89b2bdeed3afa067f9af63ad84f
itschirag-me <imchirag2000@gmail.com>
2025-11-15 17:01:06 +0530

    Refactor Vercel configuration and update main entry point. Changed source and destination paths in vercel.json from 'dist/vercel.js' to 'dist/main.js'. Updated main.ts to use process.env.PORT for listening. Removed deprecated vercel.ts file.

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(configService.get<number>(Env.PORT) ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
